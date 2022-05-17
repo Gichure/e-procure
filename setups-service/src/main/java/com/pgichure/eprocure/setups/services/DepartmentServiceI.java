dbca01f9c529b2ee9128be20ab0d461c314f6f00
Paul Gichure <boy.gich@gmail.com>
2022-05-17 15:25:53 +0300

    Create DepartmentServiceI.java

---
+++ setups-service/src/main/java/com/pgichure/eprocure/setups/services/DepartmentServiceI.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.pgichure.eprocure.setups.services;
+
+import com.pgichure.eprocure.setups.dtos.CompanyDto;
+
+/**
+ * The Department service interface
+ * @author pgichure
+ *
+ */
+public interface DepartmentServiceI{
+  
+    public DepartmentDto save(DepartmentDto departmentDto);
+  
+    public DepartmentDto findById(Long id);
+  
+    public DepartmentDto update(Long id, DepartmentDto departmentDto);
+  
+    public List<DepartmentDto> findAll();
+  
+    public DepartmentDto delete(Long id);
+  
+}
