d2ab635e35e704d8020dfbfc9284bbc2d1af5fde
Paul Gichure <boy.gich@gmail.com>
2022-05-17 15:39:16 +0300

    Update AddressDto.java

--- setups-service/src/main/java/com/pgichure/eprocure/setups/dtos/AddressDto.java
+++ setups-service/src/main/java/com/pgichure/eprocure/setups/dtos/AddressDto.java
@@ -18,7 +18,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class AddressDto {
+public class AddressDto implements Serializable{
 
 	private Long id;
 	
