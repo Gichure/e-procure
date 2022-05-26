486ad17e30c84aa1a532172792578cb5de647fa2
Paul Gichure <boy.gich@gmail.com>
2022-05-26 16:01:44 +0300

    Create PersonDto.java

---
+++ employees-service/src/main/java/com/pgichure/eprocure/employees/dtos/PersonDto.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.pgichure.eprocure.employees.dtos;
+
+import java.io.Serializable;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author pgichure
+ *
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonDto implements Serializable{
+
+	private Long id;
+  
+	private String firstName;
+	
+	private String surname;
+  
+	private Gender gender;
+	
+	private ZonedDateTime dob;
+	
+	private Long addressId;
+	
+	private Long groupId;
+	
+	private Long companyId;
+  
+}
