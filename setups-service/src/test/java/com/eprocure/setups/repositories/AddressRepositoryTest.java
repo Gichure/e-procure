4e24b5b2a282ae41e700c3056253aab48b494a11
Paul Gichure <boy.gich@gmail.com>
2022-05-31 14:05:50 +0300

    Address Repository Test

---
+++ setups-service/src/test/java/com/eprocure/setups/repositories/AddressRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.eprocure.setups.repositories;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.pgichure.eprocure.setups.models.Address;
+import com.pgichure.eprocure.setups.repositories.AddressRepository;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class AddressRepositoryTest{
+
+	@Autowired
+	AddressRepository repository;
+
+	@Test
+	public void find_no_addresses_if_repo_is_empty() {
+		Iterable<Player> players = repository.findAll();
+
+		assertThat(players).isEmpty();
+	}
+  
+  @Test
+	public void save_an_address() {
+
+	}
+  
+ 
+}
