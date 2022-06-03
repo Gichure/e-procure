c76979bedec7f9dde09f982232589accce0e7c9b
Paul Gichure <boy.gich@gmail.com>
2022-06-03 13:32:42 +0300

    Create CountryRepositoryTest.java

---
+++ setups-service/src/test/java/com/pgichure/eprocure/setups/repositories/CountryRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.pgichure.eprocure.setups.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import com.pgichure.eprocure.setups.models.Country;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+
+@DataJpaTest
+public class CountryRepositoryTest{
+	
+	@Autowired
+	private TestEntityManager entityManager;
+	
+	@Autowired
+	private CountryRepository repository;
+
+	@Test
+	public void should_find_no_records_if_repository_is_empty() {
+	    Iterable list = repository.findAll();
+	    assertThat(list).isEmpty();
+	}
+	
+	@Test
+	public void should_save() {
+	
+	}
+	
+	  @Test
+	  public void should_find_all_records() {
+	    List<Country> list = repository.findAll();
+	    assertThat(list).hasSize(3);
+	  }
+	  
+	  
+	  @Test
+	  public void should_find_record_by_id() {
+      
+	  }
+	   
+	  
+	  @Test
+	  public void should_update_by_id() {
+	    
+	  }
+	  
+	  
+	  @Test
+	  public void should_delete_record_by_id() {
+	    
+	  }
+	  
+	  
+	  @Test
+	  public void should_delete_all_records() {	    
+	    repository.deleteAll();
+	    assertThat(repository.findAll()).isEmpty();
+	  }
+}
