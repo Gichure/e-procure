ae343b8865d9da140e5183217e56279e12be86df
Paul Gichure <boy.gich@gmail.com>
2022-06-03 14:19:33 +0300

    Update SetupsServiceAppApplicationTests.java

--- setups-service/src/test/java/com/pgichure/eprocure/setups/SetupsServiceAppApplicationTests.java
+++ setups-service/src/test/java/com/pgichure/eprocure/setups/SetupsServiceAppApplicationTests.java
@@ -1,8 +1,13 @@
 package com.pgichure.eprocure.setups;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class SetupsServiceAppApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
 
 }
